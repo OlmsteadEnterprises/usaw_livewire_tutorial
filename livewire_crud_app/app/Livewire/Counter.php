0ef8548dc3963c11af27baea91103091e6a391b2
travisolmstead <travis.olmstead@healthcareintegrations.com>
2024-07-24 10:55:00 -0600

    working through laracast video 3 completed

--- livewire_crud_app/app/Livewire/Counter.php
+++ livewire_crud_app/app/Livewire/Counter.php
@@ -7,9 +7,17 @@ use Livewire\Component;
 class Counter extends Component
 {
     public $count = 1;
-
+    public $todo = '';
+    public $toDos = [
+        'take out trash',
+        'do dishes'
+    ];
     public function increment() {
         return $this->count++;
+    }
+    public function add() {
+        $this->toDos[] = $this->todo;
+        $this->reset('todo');
     }
     public function render()
     {
--- livewire_crud_app/resources/views/livewire/counter.blade.php
+++ livewire_crud_app/resources/views/livewire/counter.blade.php
@@ -2,4 +2,16 @@
     {{-- Do your work, then step back. --}}
     Counter: {{$count}}
     <button wire:click="increment">+</button>
+
+    <div>
+        <form wire:submit="add">
+            <input type="text" wire:model="todo" />
+            <button type="submit" >Add</button>
+        </form>
+        <ul>
+            @foreach($toDos as $toDo)
+                {{$toDo}}
+            @endforeach
+        </ul>
+    </div>
 </div>
